1bda83d2c8e6a37935e1ced6eafe606a6c509440
AleMiclos <alexandremiclos@gmail.com>
2024-10-23 22:56:42 -0300

    .env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 
 // Configuração do MongoDB
-mongoose.connect('mongodb://localhost:27017/gameScores', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('Conectado ao MongoDB'))
